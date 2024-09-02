a1362dcd17c22e6dbb53e0cb13d7de2908dd3540
yulafezmesi <certel@hotmail.com>
2024-09-02 12:00:19 +0300

    d

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,13 @@
 import { http, createConfig } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
+import { coinbaseWallet, injected, walletConnect, safe } from 'wagmi/connectors';
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
   connectors: [
     injected(),
     coinbaseWallet({ appName: 'Create Wagmi' }),
+    safe(),
     walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
   ],
   transports: {
